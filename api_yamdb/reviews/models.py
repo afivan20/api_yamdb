a2342e98a3417d02e172ad508018a05e5abb4865
Ivan <afivan@bk.ru>
2021-11-30 11:46:05 +0300

    added path'api/' in root urls

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,7 +3,15 @@ from django.db import models
 
 
 class User(AbstractUser):
+    username = models.SlugField('Имя пользователя', max_length=150, blank=False, unique=True)
+    email = models.EmailField('Эл. почта', blank=False)
+    first_name = models.CharField('Имя', max_length=150, blank=True)
+    last_name = models.CharField('Фамилия', max_length=150, blank=True)
     bio = models.TextField(
         'Биография',
         blank=True,
     )
+    #role = models.TextField()
+
+    def __str__(self):
+        return self.username
