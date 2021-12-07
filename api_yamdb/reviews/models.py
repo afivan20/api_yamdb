4b07e04e3f98fdc29c7606b990d7fe6e1c69a843
Ivan <afivan@bk.ru>
2021-12-07 23:11:57 +0300

    polished User model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
     class UserRoles(models.TextChoices):
-        USER = 'user', _('user')
-        ADMIN = 'admin', _('admin')
-        MODERATOR = 'moderator', _('moderator')
+        USER = 'user'
+        ADMIN = 'admin'
+        MODERATOR = 'moderator'
 
     username = models.SlugField(
         'Имя пользователя',
